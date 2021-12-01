5c2a59000f2eb2b44c4df1e52682e69c44c5108e
андрей якуньков <andreyiakunkov@mail.ru>
2021-12-01 17:43:29 +0300

    Промежуточные итоги, Победители номинаций

--- server/index.js
+++ server/index.js
@@ -21,6 +21,14 @@ const jsonParse = express.json();
 
 app.use(express.static(path.resolve(__dirname, '../client/build')));
 
+app.use(function (request, response, next) {
+    console.log(formDate(request));
+    //response.set('Content-Type', 'text/plain');
+
+    //fs.appendFile("server.log", data + "\n", function(){});
+    next();
+});
+
 app.get("/api", (req, res) => {
   res.json({ message: "Hello from server!" });
 });
@@ -103,11 +111,78 @@ app.get("/api/nominations", (req, res) => {
       })
 })
 
-//Добавить номинацию(сейчас не добавляет)
+//Добавить номинацию
 app.post("/api/nominations/add", jsonParse, (req, res)=>{
-    console.log(req.body);
+    //console.log(req.body);
 
-    return res.send('Ничего не произошло, но ты всё равно молодец');
+    if (!req.body['nominationName'] || typeof(req.body['nominationName']) !== 'string'){
+        console.log('Номинация не является строкой');
+        return res.status(400).send({message: 'rejected'});
+    }
+
+    const title = req.body['nominationName'];
+
+    clientPg.query('insert into "Nominations" ("title") values ($1)',
+        [title])
+        .then(result => {
+            console.log(`Затронуто строк: ${result.rowCount}`);
+            console.log(`Номинация '${title}' успешно добавлена`);
+            res.send({status: true});
+        })
+        .catch(err => {
+            console.log('Ошибка');
+            if (err.code === '23505'){
+                console.error(`Номинация '${title}' уже существует`);
+                return res.status(401).send({status: 'already exist'});
+            }
+            console.error(err);
+            return res.status(500).send({status: 'unknown error'});
+        })
+
+    //return res.send('Ничего не произошло, но ты всё равно молодец');
+})
+
+//промежуточные итоги
+app.get("/api/nominations/result", (req, res) => {
+
+    clientPg.query("select nomination, name, surname, patronymic, \"countVotes\" from \"Candidates\"\n" +
+        "order by nomination, \"countVotes\" desc")
+        .then(result => {
+            console.log(`Отпрввлено ${result['rows'].length} записей`);
+            res.send(result['rows']);
+        })
+        .catch(err => {
+            console.log('Ошибка');
+            console.error(err);
+            res.status(500).send({status: 'unknown error'});
+        })
+})
+
+//Получить победителей в номинации
+app.get("/api/nominations/winners", (req, res) => {
+    clientPg.query(
+        "select nomination, name, surname, patronymic, \"countVotes\" from \"Candidates\" as c1" +
+        "   where " +
+        "   \"countVotes\" in " +
+        "       (select max(\"countVotes\") from \"Candidates\" as c2 " +
+        "           where c1.\"nomination\" = c2.\"nomination\")")
+        .then(result => {
+            console.log(`Отпрввлено ${result['rows'].length} записей`);
+            res.send(result['rows']);
+
+            // let winners = [];
+            // for (let i = 0; i < result['rows'].length; ++i){
+            //     let winner = {
+            //         nomination: result['rows'][i]['nomination'],
+            //
+            //     }
+            // }
+        })
+        .catch(err => {
+            console.log('Ошибка');
+            console.error(err);
+            res.status(500).send({status: 'unknown error'});
+        })
 })
 
 //Тест логина в гугле
@@ -132,3 +207,23 @@ app.listen(PORT, () => {
     console.log('Связь с бд установлена');
   });
 });
+
+//Формирует строку запроса для логгирования
+function formDate(request) {
+    let now = new Date();
+    //let day = now.getDay();
+    //let month = now.getMonth();
+    //let year = now.getUTCFullYear();
+    let hour = now.getHours();
+    let minutes = now.getMinutes();
+    let seconds = now.getSeconds();
+    return `${hour}:${minutes}:${seconds} ${request.method} [[${request.url}]] ${request.get("user-agent")}`;
+    //let login = request.session.login ? request.session.login : 'Неавторизован';
+    //if (!request.session.login) request.session.login = 'Неавторизован';
+    // if (force) {
+    //     return `${day}.${month}.${year} ${hour}:${minutes}:${seconds} ${request.method} [[${request.url}]]` +
+    //         ` ${request.session.login} (${request.sessionID}) ${request.get("user-agent")}`;
+    // }
+    // return `${hour}:${minutes}:${seconds} ${request.method} [[${request.url}]]` +
+    //     ` ${request.session.login} (${request.sessionID}) ${request.get("user-agent")}`;
+}
