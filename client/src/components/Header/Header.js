0d6aab7d510925b1dbcad1907667a1dcbbe75c21
akiyoss <90448564+profkomnntudev@users.noreply.github.com>
2021-12-07 15:48:03 +0300

    update button

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -8,12 +8,19 @@ class Header extends React.Component{
       }
 
     render() {
+        renderProps = {
+            disabled: false,
+            onClick: ()=>console.log("YAY")
+        }
         return(
             <div>
                 Header
                 <GoogleLogin
                     clientId="1040836497320-5l8asulqvqgv0a01jp6caoesdc7a59lq.apps.googleusercontent.com"
                     buttonText="Войти через Google"
+                    render={renderProps => (
+                        <button onClick={renderProps.onClick} disabled={renderProps.disabled}>This is my custom Google button</button>
+                      )}
                     onSuccess={this.responseGoogle}
                     onFailure={this.responseGoogle}
                     cookiePolicy={'single_host_origin'}
