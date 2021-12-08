bb3a4b8483ff1168b32516d3417870257c3a6aaf
akiyoss <90448564+profkomnntudev@users.noreply.github.com>
2021-12-08 07:24:25 +0300

    add all hrefs и вообще я заебался пойду спать

--- client/src/components/Nominations/Nominations.js
+++ client/src/components/Nominations/Nominations.js
@@ -4,18 +4,66 @@ import "./Nominations.css"
 
 class Nominations extends React.Component{
     render(){
-        let students = ["АКТИВИСТ ГОДА", "ПОЛИТЕХНИК ГОДА", "САМЫЙ УМНЫЙ", "САМЫЙ ИЗОБРЕТАТЕЛЬНЫЙ", "СТУДЕНТ ГОДА"];
-        let teachers = ["САМЫЙ СТИЛЬНЫЙ", "ЛУЧШИЙ ЛЕКТОР", "САМЫЙ ПОЗИТИВНЫЙ", "САМЫЙ ИННОВАЦИОННЫЙ", "ПРЕПОДАВАТЕЛЬ ГОДА"];
-
+        let students = [
+            {
+                title: "АКТИВИСТ ГОДА", 
+                link: "activeOfTheYear"
+            },
+            {
+                title: "ПОЛИТЕХНИК ГОДА",                
+                link: "politehnikOfTheYear"
+            }, 
+            {
+                title: "САМЫЙ УМНЫЙ",                
+                link: "theMostKind"
+            }, 
+            {
+                title: "САМЫЙ ИЗОБРЕТАТЕЛЬНЫЙ",                
+                link: "theMostInventive"
+            }, 
+            {
+                title: "СТУДЕНТ ГОДА",
+                link: "sportsmanOfTheYear"
+            }
+        ];
+        let teachers = [
+            {
+                title: "САМЫЙ СТИЛЬНЫЙ",                
+                link: "theMostStylish"
+            }, 
+            {
+                title: "ЛУЧШИЙ ЛЕКТОР",                
+                link: "bestLector"
+            }, 
+            {
+                title: "САМЫЙ ПОЗИТИВНЫЙ",                
+                link: "theMostPositive"
+            }, 
+            {
+                title: "САМЫЙ ИННОВАЦИОННЫЙ",                
+                link: "theMostInnovative"
+            }, 
+            {
+                title: "ПРЕПОДАВАТЕЛЬ ГОДА",                
+                link: "teacherOfTheYear"
+            },
+        ];
         let used = this.props.nominants ? this.props.nominants : this.props.isStudent ? students : teachers
         let buttonText = this.props.nominants ? "Проголосовать" : "Открыть"
         return (
             <div className="nominations">
-                <div className="text">
+                {!this.props.nominants && <div className="text">
                     {this.props.isStudent ? "Номинации для студентов" : "Номинации для преподавателей"}
-                </div>
+                </div>}
                 <div className="formBlock">
-                    {used.map((title) => <div className="forms"><Nomination label={title} buttonText={buttonText}/></div>)}
+                    {used.map((item) => <div className="forms"><Nomination label={item.title || item} buttonText={buttonText} onClick={() => {
+                        if (!this.props.nominants){
+                            window.location.href = "/vote/"+item.link
+                        } else {
+                            //here voting function
+                            alert("ТУТ НОМИНАНТЫ НИКУДА НЕ ПЕРЕЙДЕШЬ")
+                        }
+                    }}/></div>)}
                 </div>
                 
             </div>
