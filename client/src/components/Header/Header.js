1e4fb2003f81648e7c7b95305af022a9bb902c80
akiyoss <90448564+profkomnntudev@users.noreply.github.com>
2021-12-09 19:05:50 +0300

    add logout

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -1,14 +1,29 @@
 import React from 'react';
-import {GoogleLogin} from 'react-google-login';
+import {GoogleLogin, GoogleLogout} from 'react-google-login';
 import CustomButton from '../CustomButton/CustomButton';
 import logo from "../../static/logo.svg"
 import "./Header.css"
 import axios from "axios";
 
 class Header extends React.Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            'loggedIn': false
+        }
+    }
 
     responseGoogle = (response) => {
         console.log(response);
+        console.log(response.profileObj)
+        //добавляем куки access_token
+        //посылаем на бек запрос с acess_token
+        this.setState({'loggedIn': true})
+    }
+
+    logout = (response) => {
+        //чистим куки от access_token
+        this.setState({'loggedIn': false})
     }
 
 
@@ -21,9 +36,9 @@ class Header extends React.Component {
         return (
             <div className="header">
                 <img src={logo} className={"logo"}/>
+                {!this.state.loggedIn ? 
                 <GoogleLogin
                     clientId="1040836497320-5l8asulqvqgv0a01jp6caoesdc7a59lq.apps.googleusercontent.com"
-                    buttonText="Войти через Google"
                     render={renderProps => (
                         <CustomButton text={"Войти"} disabled={renderProps.disabled} onClick={renderProps.onClick}
                                       style={buttonStyle}/>
@@ -31,7 +46,18 @@ class Header extends React.Component {
                     onSuccess={this.responseGoogle}
                     onFailure={this.responseGoogle}
                     cookiePolicy={'single_host_origin'}
-                />
+                /> 
+                :
+                <GoogleLogout
+                    clientId="1040836497320-5l8asulqvqgv0a01jp6caoesdc7a59lq.apps.googleusercontent.com"
+                    buttonText="Logout"
+                    render={renderProps => (
+                        <CustomButton text={"Выйти"} disabled={renderProps.disabled} onClick={renderProps.onClick}
+                                      style={buttonStyle}/>
+                    )}
+                    onLogoutSuccess={this.logout}
+                    >
+                </GoogleLogout>}
             </div>
         )
     };
