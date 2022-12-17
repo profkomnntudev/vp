cc66bee3af9cb87d99c3c00c7f87c086f9fe401f
akiyoss <sawerford10@gmail.com>
2022-12-17 03:13:31 +0300

    Update Vote.js

--- client/src/components/Vote/Vote.js
+++ client/src/components/Vote/Vote.js
@@ -91,7 +91,7 @@ class Votee extends React.Component{
                 for(let i=0;i<res.data.length;i++)
                 {
                     //candidate = {name:res.data[i].name, surname:res.data[i].surname,patronymic: res.data[i].patronymic};
-                    nominee.push({'name': res.data[i].name+" "+res.data[i].surname, 'id': res.data[i].id, 'img': res.data[i].img, 'story': res.data[i].about});
+                    nominee.push({'name': res.data[i].surname || "" + " "+ res.data[i].name + res.data[i].patronymic || "", 'id': res.data[i].id, 'img': res.data[i].img, 'story': res.data[i].about});
                 }
                 this.setState({nominants: nominee});
             })
